9d2e07298d826ec6eec2c4b8aae721e844579a08
Prasad <prasad.vasaikar@gmail.com>
2018-04-10 19:18:27 +0530

    Changes 10/04/2018 Get Location Value

--- app/src/main/java/com/example/user/test_2018/MainActivity.java
+++ app/src/main/java/com/example/user/test_2018/MainActivity.java
@@ -1,13 +1,150 @@
 package com.example.user.test_2018;
 
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    String LOCATION_PROVIDER = LocationManager.NETWORK_PROVIDER;
+    LocationManager mLocationManager;
+    LocationListener mLocationListener;
+    boolean mLocationPermissionGranted;
+    final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION=123;
+    final long MIN_TIME = 5000;
+    // Distance between location updates (1000m or 1km)
+    final float MIN_DISTANCE = 1000;
+    double longitude;
+    double latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Check location Permission
+        getLocationPermission();
+
+        //Get Current Location
+        getCurrentLocation();
+
+        //Get City Name
+        getCityName();
+        //Search Button
+        ImageButton search = (ImageButton)findViewById(R.id.search);
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get Current Weather Report
+            }
+        });
     }
-}
+
+    private void getLocationPermission() {
+    /*
+     * Request location permission, so that we can get the location of the
+     * device. The result of the permission request is handled by a callback,
+     * onRequestPermissionsResult.
+     */
+        if (ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        mLocationManager.requestLocationUpdates(LOCATION_PROVIDER, MIN_TIME, MIN_DISTANCE, mLocationListener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+        //Get Current Location
+        getCurrentLocation();
+    }
+
+    public void getCurrentLocation()
+    {
+        if(mLocationPermissionGranted=true) {
+            mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            mLocationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+
+                    longitude = location.getLongitude();
+                    latitude = location.getLatitude();
+                }
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                    //Log.d("Clima", "onProviderDisabled() callback received");
+                }
+
+            };
+
+
+        }
+        else
+        {
+
+        }
+    }
+
+    public void getCityName()
+    {
+        EditText city= (EditText) findViewById(R.id.editText);
+        try
+        {
+            Geocoder gcd = new Geocoder(MainActivity.this, Locale.getDefault());
+            List<Address> addresses = gcd.getFromLocation(latitude, longitude, 1);
+            if (addresses.size() > 0)
+                city.setText(addresses.get(0).getLocality());
+        }
+        catch (IOException e)
+        {
+
+        }
+
+    }
+
+
+    }
